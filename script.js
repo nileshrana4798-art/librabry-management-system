db6f8173e6633e686426c9e4329fedce1c673a95
nileshrana4798-art <nileshrana4798@gmail.com>
2025-12-24 13:31:48 +0530

    Refactor user registration and login functions

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // REGISTER
 function registerUser() {
-    let name = rname.value;
-    let email = remail.value;
-    let password = rpassword.value;
+    let user = {
+        name: rname.value,
+        email: remail.value,
+        password: rpassword.value
+    };
 
-    if (!name || !email || !password) {
-        alert("All fields required");
+    if (!user.name || !user.email || !user.password) {
+        alert("Fill all fields");
         return;
     }
 
-    localStorage.setItem("user", JSON.stringify({ name, email, password }));
+    localStorage.setItem("user", JSON.stringify(user));
     alert("Registration Successful");
     window.location.href = "login.html";
 }
 
 // LOGIN
 function loginUser() {
-    let email = lemail.value;
-    let password = lpassword.value;
     let user = JSON.parse(localStorage.getItem("user"));
 
     if (!user) {
-        alert("Please register first");
+        alert("Register first");
         return;
     }
 
-    if (email === user.email && password === user.password) {
+    if (lemail.value === user.email && lpassword.value === user.password) {
         alert("Login Successful");
         window.location.href = "books.html";
     } else {
@@ -33,27 +33,37 @@ function loginUser() {
     }
 }
 
-// BOOKS
+// BOOK DATA
 let books = [
-    { name: "JavaScript Basics", author: "John Smith", img: "https://covers.openlibrary.org/b/id/10521270-L.jpg" },
-    { name: "HTML & CSS", author: "Mark Allen", img: "https://covers.openlibrary.org/b/id/10909258-L.jpg" },
-    { name: "Python Programming", author: "David Ross", img: "https://covers.openlibrary.org/b/id/11153241-L.jpg" }
+    {
+        name: "JavaScript Basics",
+        author: "John Smith",
+        img: "https://covers.openlibrary.org/b/id/10521270-L.jpg"
+    },
+    {
+        name: "HTML & CSS",
+        author: "Mark Allen",
+        img: "https://covers.openlibrary.org/b/id/10909258-L.jpg"
+    },
+    {
+        name: "Python Programming",
+        author: "David Ross",
+        img: "https://covers.openlibrary.org/b/id/11153241-L.jpg"
+    }
 ];
 
 function loadBooks() {
     let list = document.getElementById("bookList");
     if (!list) return;
 
-    books.forEach(book => {
-        let div = document.createElement("div");
-        div.className = "book";
-        div.innerHTML = `
-            <img src="${book.img}">
-            <h3>${book.name}</h3>
-            <p>${book.author}</p>
-            <button onclick="alert('Book Issued')">Issue</button>
+    books.forEach(b => {
+        list.innerHTML += `
+            <div class="book">
+                <img src="${b.img}">
+                <h3>${b.name}</h3>
+                <p>${b.author}</p>
+            </div>
         `;
-        list.appendChild(div);
     });
 }
 
